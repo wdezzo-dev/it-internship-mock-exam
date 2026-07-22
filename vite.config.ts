a9615f365c1aa68732e56d0cf12480ec76b4d99f
Abdalla Izzeldin <187668492+wdezzo-dev@users.noreply.github.com>
2026-07-22 16:23:12 +0200

    Change alias path in Vite config to current directory

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,11 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "."),
     },
   },
 });
